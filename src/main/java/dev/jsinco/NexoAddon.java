d7484fe7ac8a83705b046d7ad01cb33bf587a6e5
finder1793 <kangas.kyle@gmail.com>
2024-12-07 14:13:33 -0600

    1

--- src/main/java/dev/jsinco/NexoAddon.java
+++ src/main/java/dev/jsinco/NexoAddon.java
@@ -2,7 +2,7 @@ package dev.jsinco;
 
 import com.dre.brewery.BreweryPlugin;
 import com.dre.brewery.api.addons.AddonFileManager;
-import com.dre.brewery.api.addons.AddonLogger;
+
 import com.dre.brewery.api.addons.BreweryAddon;
 import com.dre.brewery.recipe.PluginItem;
 import org.bukkit.Bukkit;
@@ -10,12 +10,9 @@ import org.bukkit.Bukkit;
 public class NexoAddon extends BreweryAddon {
 
 
-    private static AddonLogger logger;
+
     private static boolean useNexo = false;
 
-    public NexoAddon(BreweryPlugin plugin, AddonLogger logger) {
-        super(plugin, logger);
-    }
 
     @Override
     public void onAddonEnable(AddonFileManager addonFileManager) {
@@ -25,9 +22,6 @@ public class NexoAddon extends BreweryAddon {
             useNexo = Bukkit.getPluginManager().getPlugin("Nexo") != null;
 
             
-            if (useNexo) {
-                logger.info("Nexo support enabled!");
-            }
         }, 1L);
     }
 
